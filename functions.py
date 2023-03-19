5da50011a052134e8e883456d0b2a201d621b7c9
Yashpreet Kaur Virk <69677307+DefaultSpieler@users.noreply.github.com>
2023-03-19 21:01:09 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -150,6 +150,139 @@ def getEven(a):
          b.append(i)
    return b
 
+#Write a Python program to find biggest of THREE numbers using function.
+
+def find_greatest(a,b,c):
+	if a == b and b == c and c == a:
+		return "All the values are same"
+	elif a > b and a > c:
+		return f"{a} is the greatest value"
+	elif b > a and b > c:
+		return f"{b} is the greatest"
+	else:
+		return f"{c} is the greatest"
+
+print(find_greatest(10,5,9))
+
+#Write a Python program using functionthat return absolute value of number.
+
+def absolut(a):
+	if a < 0:
+		return -a
+	else:
+		return a
+
+print(absolut(5))
+
+#Write a Python function to sum all the numbers in a list.
+
+a = [1,2,4,5,7,8,9,3]
+add = 0
+def sum_of(a):
+	global add
+	for item in a:
+		add = add + item
+	return add
+
+print(sum_of(a))
+print(add)
+
+# Write a Python program to reverse a string.
+
+a = [1,2,4,5,7,8,9,3]
+
+def aa(a):
+	return a[::-1]
+
+b = lambda a: a[::-1]
+print(b(a))
+
+#Write a Python function to check whether a number is in a given range
+
+start = 0
+end = 5
+num = 1
+
+def check_in(s,e,n):
+	if n > s and n < e:
+		return True
+
+print(check_in(start, end, num))
+
+#Write a Python program to print the even numbers from a given list.
+
+a = [1,8,7,-6,2,6,9,-8]
+
+def ch(a):
+	b = []
+	b = [i for i in a if i > 0]
+	return b
+
+print(ch(a))
+
+#Write a Python program that uses lambda function to multiply two numbers
+
+a = lambda x,y : x * y
+
+print(a(5,2))
+
+# Write a Python program that passes lambda function as an argument to another function to compute the cube of a number
+
+a = lambda x : x ** 3
+print(a(3))
+
+#Write a Python program using function is_perfect() that returns value 1 if the argument passed to it is a perfect number and 0 otherwise.
+
+def is_perfect(x):
+	add = 0
+	for i in range(1,x):
+		if x % i == 0:
+			add = add + i
+	if add == x:
+		return 1
+
+print(is_perfect(6))
+
+#Write a Python program to calculate the area of a triangle using a function.
+
+def cal_area(b,h):
+	area = 1/2 * (b * h)
+	return area
+
+print(cal_area(5,4))
+
+#.A function that accepts two values and find their sum
+
+def cal_sum():
+	a = 5
+	b = 10
+	return a + b
+
+print(cal_sum())
+
+#A python program to find the sum of two numbers and return the result from the function
+
+#A function to test whether a number is even or odd
+
+def num(a):
+	if a % 2 == 0:
+		return True
+	else:
+		return False
+
+print(num(4))
+
+#A python program to calculate factorial values of numbers
+
+def find_fact(a):
+	if a == 1:
+		return 1
+	else:
+		return a * find_fact(a - 1)
+
+print(find_fact(5))
+
+#.A python program to check whethergiven number is prime or not
 #Write a Python function to check whether a number is perfect or not. Go to the editor
 #According to Wikipedia : In number theory, a perfect number is a positive integer that is equal to the sum of its 
 #proper positive divisors, that is, the sum of its positive divisors excluding the number itself (also known as its 
